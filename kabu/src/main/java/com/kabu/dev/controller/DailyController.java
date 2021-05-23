28fead7bd59434e2e16c16f1d88d1767698af294
Yikeyang0517 <yikeyang0517@yahoo.co.jp>
2021-05-23 14:13:15 +0900

    0523 yang

--- kabu/src/main/java/com/kabu/dev/controller/DailyController.java
+++ kabu/src/main/java/com/kabu/dev/controller/DailyController.java
@@ -1,12 +1,17 @@
 package com.kabu.dev.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kabu.dev.dao.DailyEntityMapper;
+import com.kabu.dev.dto.DailyOutDto;
 import com.kabu.dev.dto.KLineOutDto;
+import com.kabu.dev.dto.UserCollectionDto;
 import com.kabu.dev.service.DailyService;
 import com.kabu.dev.vo.ResultJson;
 
@@ -16,6 +21,9 @@ import com.kabu.dev.vo.ResultJson;
 public class DailyController {
 	@Autowired
 	DailyService dailyService;
+	@Autowired
+	DailyEntityMapper dailyDao;
+	
 	
 	@GetMapping("kline")
 	public ResultJson getkline(String stockId,String day) {
@@ -29,6 +37,30 @@ public class DailyController {
 			ResultJson ret=ResultJson.fail(e);
 			return ret;
 		}
+		
+		
+	}
+	@GetMapping("insert")
+	public String createUserCollection(UserCollectionDto dto) {
+		try{
+			dailyService.createUserCollection(dto);
+			return  "気に入りました";
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return  "気に入らない";
+			}
+		
+		
+	}
+	@GetMapping("collectionList")
+	public List<DailyOutDto> listAll() {
+		String userId = "admin";
+		//TODO
+		//从session中获取userId
+		List<DailyOutDto> list = dailyDao.selectmyfavourite(userId);
+		return list;
+		
 		
 		
 	}
